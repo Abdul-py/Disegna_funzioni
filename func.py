22008401a6e2532e4bdc0fcafa07a026755e5657
Abdullah Ijaz <abdullahijaz2912@gmail.com>
2022-12-07 18:31:54 +0100

    Creating a function to get an equation as input from the user

--- func.py
+++ func.py
@@ -1,40 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 import sympy as sp
 
+sns.set_style("whitegrid")
 
-def ask_func():
-    funcs = {}
-    
-    n_f = int(input("Inserisci in numero di funzioni: "))
-    if n_f < 1:
-        exit()
-    for i in range(1, n_f + 1):
+# Setting the operators
+sqrt = np.sqrt
+log = np.log
+abs = np.abs
+sin = np.sin
+cos = np.cos
+
+def eq():
+    print("Inserisci il tipo di equazione:")
+    print(r"1. Intera")
+    print(r"2. Frazionaria")
+    tipo = str(int(input("Inserisci il numero corrispondente: ")))
+    if tipo == "1":
         try:
-            f = input("Inserisci la funzione: ")
-            nome = f'func_{i}'
-            funcs[nome] = f
-        except:
-            print("Funzione non valida")
-            ret = str(input("Vuoi riprovare? (y/n): "))
-            if ret == 'y':
-                ask_func()
-            else:
-                exit()
-        return funcs
-        
-
-        
-        
+            eqaz = str(input("Inserisci l'equazione: ")) 
+            return eqaz
+        except ValueError:
+            print("Inserisci una equazione valida")
+            eq()
+            
+    elif tipo == "2":
+        try:
+            num = str(input("Inserisci il numeratore dell'equazione: "))
+            den = str(input("Inserisci il denominatore dell'equazione: "))
+            equz = "(" + num + ")" + "/" + "(" + den + ")"
+            return equz
+        except ValueError:
+            print("Inserisci una equazione valida")
+            eq()
+            
 def main():
-    functions = ask_func()
-    x = sp.Symbol('x')
-    y = sp.Symbol('y')
-    if functions:
-        print(functions)
-        p = sp.plot(functions['func_1'], show=False)
-        print(p)
-        p.show()
-        
+    print(eq())
     
 if __name__ == "__main__":
     main()
-    
